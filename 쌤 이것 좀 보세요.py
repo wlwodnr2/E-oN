b10dffef69b09d8cc8c835b610b53fbea05c63b4
wlwodnr2 <wlwodnr2>
2020-05-23 11:06:27 +0900

    Create 쌤 이것 좀 보세요.py

---
+++ 쌤 이것 좀 보세요.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+james = Person("지재욱", 24, "수원")
+james.greeting()
+
+class calculator:
+    def _init_(self):
+        self.result = 0
+    
+    def add(self, num):
+        self.result += num
+        return self.result
+    
+    
